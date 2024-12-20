76bfc1a5323629e40152c075941b075506841727
Judit <juditalcaideru@gmail.com>
2024-12-21 00:51:06 +0100

    Descripción de personalidad/personaje en el apartado del resultado añadida y estilizada con el contenido

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const preguntasDiv = document.getElementById('preguntas');
 const resultadoDiv = document.getElementById('resultado');
 const personajeSpan = document.getElementById('personaje');
 const imgSpan = document.getElementById('imgPersonaje');
+const descripcionPersonaje = document.getElementById('descripcionPersonaje');
 
 let puntosTotales = 0;
 let preguntaActual = 0;
@@ -43,19 +44,24 @@ function mostrarResultado() {
 
     let personaje;
     let imagen;
+    let descripcion;
 
     if (puntosTotales < 30) {
         personaje = "Donna";
         imagen = "https://i.postimg.cc/FzjZ2dN9/Donna.jpg";
+        descripcion = "Eres como Donna Sheridan, una persona independiente, apasionada y con un gran corazón. Te enfrentas a la vida con valentía y no temes tomar decisiones difíciles por el bien de las personas que amas. Tu energía y autenticidad inspiran a quienes te rodean, y aunque a veces cargas mucho sobre tus hombros, nunca pierdes esa chispa que te hace única.";
     } else if (puntosTotales < 50) {
         personaje = "Tanya";
         imagen = "https://i.postimg.cc/qBnjtYn9/Tanya.jpg";
+        descripcion = "Eres como Tanya Chesham-Leigh, sofisticada, segura de ti misma y con un gran sentido del humor. Disfrutas de los placeres de la vida y no te tomas las cosas demasiado en serio. Sabes lo que quieres y cómo conseguirlo, y aunque puedes parecer despreocupada, siempre estás ahí para tus amigas cuando te necesitan.";
     } else {
         personaje = "Rosie";
         imagen = "https://i.postimg.cc/4NQPP9WP/Rosie.jpg";
+        descripcion = "Eres como Rosie Mulligan, una persona divertida, amable y llena de creatividad. Siempre sabes cómo alegrar el día de los demás con tu humor y tus ocurrencias. Te encanta vivir el momento y no necesitas seguir las expectativas de los demás para ser feliz. Tu lealtad y espontaneidad te convierten en un pilar en tu grupo de amigas.";
     }
     personajeSpan.textContent = personaje;
     if (imgSpan) {
         imgSpan.src = imagen;
     }
+    descripcionPersonaje.textContent = descripcion;
 }
